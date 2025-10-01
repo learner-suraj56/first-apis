d4dfdc5892a6606789149d5673509186f2a1971d
Suraj Singh <suraj.singh@hawkmartech.com>
2025-10-01 13:36:53 +0530

    added joi validation for createuser and updateuser without middlewares

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,17 @@
 import User from "../models/users.js";
+import Joi from "joi";
+
+const createUserSchema = Joi.object({
+    firstname: Joi.string().min(2).max(30).required(),
+    lastname: Joi.string().min(2).max(30).required(),
+    age: Joi.number().integer().min(1).max(120).required(),
+  });
+  
+  const updateUserSchema = Joi.object({
+    firstname: Joi.string().min(2).max(30),
+    lastname: Joi.string().min(2).max(30),
+    age: Joi.number().integer().min(1).max(120),
+  }).min(1);
 
 export const getUsers = async (req, res) => {
     const users = await User.find();
@@ -6,6 +19,10 @@ export const getUsers = async (req, res) => {
 }
 
 export const createUser = async (req, res) => {
+    const { error} = createUserSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
     const user = new User(req.body);
     await user.save();
     res.status(201).json(user);
@@ -17,6 +34,10 @@ export const getUser = async (req, res) => {
 }
 
 export const updateUser = async (req, res) => {
+    const { error} = updateUserSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
     const user = await User.findByIdAndUpdate(req.params.id, req.body);
     res.json(user);
 }
